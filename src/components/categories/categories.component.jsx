3e9c0f1b9ea06e85f679550bc84e3aaddf9f6484
Tariq Khan <71902492+tariq-k-dev@users.noreply.github.com>
2023-02-19 23:22:21 -0800

    Add categories and category-item components

---
+++ src/components/categories/categories.component.jsx
@@ -0,0 +1,13 @@
+import CategoryItem from '../category-item/category-item.component';
+
+import './categories.styles.scss';
+
+const Categories = ({ categories }) => (
+  <div className='categories-container'>
+    {categories.map(category => (
+      <CategoryItem key={`${category.id}`} category={category} />
+    ))}
+  </div>
+);
+
+export default Categories;
